e84c3e1963f0a7653cd19eaefaae2aa1f4055696
asaleminiK <ameen.saleminik@sjsu.edu>
2019-05-12 14:37:30 -0700

    added accessMem fucntion

--- main.c
+++ main.c
@@ -40,11 +40,13 @@ bool remove_translation(main_mem *, int page_amnt, int vir_page);
 
 bool accessMemory(page_table *, main_mem *, int vir_page, char operation);
 
+bool allocateMemory(page_table *, swap_mem *, main_mem *, int vir_page, char operation);
+
 void main_to_swap(page_table *, swap_mem *, main_mem *, int index);
 
-void swap_to_page(page_table *, swap_mem *, main_mem *, int index);
+void swap_to_main(page_table *, swap_mem *, main_mem *, int index);
 
-void clear_pages(page_table *, swap_mem *, main_mem *, int page_amt);
+void clear_pages(swap_mem *, main_mem *, int page_amt);
 
 //void run_processes(char * file_name, void(*swag_alg))
 void FIFO_swap(char *file_name, main_mem *, swap_mem *swap);
@@ -53,19 +55,17 @@ void LRU_swap(char *file_name, main_mem *, swap_mem *swap);
 
 void random_swap(char *file_name, main_mem *, swap_mem *swap);
 
-int main() {
+int main(){
 
     main_mem *main_memory = (main_mem *) (malloc(sizeof(main_mem)));
     main_memory->p_id = ((int *) malloc(sizeof(int) * PAGE_AMT));
     main_memory->vir_page = ((int *) malloc(sizeof(int) * PAGE_AMT));
     main_memory->pages = ((page_mem *) malloc(sizeof(page_mem) * PAGE_AMT));
-    main_memory->p_id[0] = 2;
 
     swap_mem *swap_memory = (swap_mem *) (malloc(sizeof(swap_mem)));
     swap_memory->p_id = ((int *) malloc(sizeof(int) * SWAP_MEMORY_AMT));
     swap_memory->vir_page = ((int *) malloc(sizeof(int) * SWAP_MEMORY_AMT));
     swap_memory->pages = ((page_mem *) malloc(sizeof(page_mem) * SWAP_MEMORY_AMT));
-    swap_memory->p_id[0] = 2;
 
 
     char file_name[32] = "memory.dat";
@@ -93,7 +93,6 @@ void random_swap(char *file_name, main_mem *main_memory, swap_mem *swap_memory) 
         token = strtok(line, " \t\n");
         incrementer = 0;
         while (token != NULL) {
-
             if (incrementer == 0) {
                 p_id = atoi(token);
                 incrementer++;
@@ -113,3 +112,49 @@ void random_swap(char *file_name, main_mem *main_memory, swap_mem *swap_memory) 
         }
     }
 }
+
+//only use for reading/writing
+bool accessMemory(page_table * page_tb, main_mem * main_m, int vir_page, char operation){
+    if(!(page_tb->present[vir_page] && page_tb->valid))
+        return false;
+    if(operation == 'R') {
+        //just read the data, no need to do anything with it
+        int temp = main_m->pages[page_tb->translations[vir_page]].data;
+        return true;
+    }
+    else if(operation == 'W'){
+        main_m->pages[page_tb->translations[vir_page]].data = 1;
+        main_m->pages[page_tb->translations[vir_page]].dirty = true;
+        return true;
+    }
+    else return false;
+}
+
+//only use for allocating and deallocate memory
+//Allocation: TRUE if allocated, FALSE if no memory OR modified pages left (SWAP MUST BE USED)
+//Allocation: SWAP occurs when all pages full but at least one page remains unmodified
+//De-allocation: TRUE if freed, FALSE if access was not granted or page is in SWAP
+/*
+bool allocateMemory(page_table * page_tb, swap_mem * swap_m, main_mem * main_m, int vir_page, char operation){
+    int index = -1;
+    if(operation == 'A'){
+        for(int i = 0; i < PAGE_AMT; i ++){
+            if (main_m->p_id[i] == -1){
+                main_m->p_id[i] = page_tb->p_id;
+                main_m->vir_page[i] = vir_page;
+                main_m->pages[i].dirty = false;
+                main_m->pages[i].data = 0;
+                return true;
+            }
+            else if(!(main_m->pages[i].dirty)){
+                index = i;
+            }
+        }
+        if(index > -1){
+
+        }
+        else return false;
+    }
+    else if(operation == 'F' || opeation == T)
+}
+ */
