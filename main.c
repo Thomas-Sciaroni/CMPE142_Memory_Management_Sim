e954d6af74493a4f81570c9790e47c60980958b7
Thomas Sciaroni <sciaroni@ieee.org>
2019-05-14 21:52:06 -0700

    turned everything into linked lists and broke a lot of stuff

--- main.c
+++ main.c
@@ -16,6 +16,7 @@ typedef struct _main_mem {
     int *data;
     bool *dirty;
     bool *accessed;
+    bool *inSwap;
 } main_mem;
 
 typedef struct _swap_mem {
@@ -24,64 +25,74 @@ typedef struct _swap_mem {
     int data;
 } swap_mem;
 
-typedef struct node {
+typedef struct _node_sm {
     swap_mem data;
-    struct node * next;
+    struct _node_sm * next;
 } node_sm;
+
+typedef struct _translation {
+    int virtualPage;
+    int physicalPage;
+    bool valid;
+    bool present;
+} translation;
+
+typedef struct _node_t {
+    translation data;
+    struct _node_t * next;
+} node_t;
 
 typedef struct _page_table {
     int p_id; // process ID for page table
-    int *translations; // array of translations
-    bool *valid; // is this a valid translation (TRUE if Valid, FALSE if not)
-    bool *present; // is the page swapped (TRUE if not swapped, FALSE if swapped)
+    node_t* head;
 } page_table;
 
-void add_translation(page_table *, int vir_page, int phys_page);
+typedef struct _process_list {
+    page_table* pageTable;
+    struct _process_list* next;
+} process_list;
+
+typedef struct _LRU_queue {
+    int physicalPage;
+    struct _LRU_queue * next;
+} node_lq;
+
+void mainInit(main_mem *);
+
+void swapInit(node_sm *);
+
+void mainDealloc(main_mem *);
+
+void swapDealloc(node_sm *);
+
+void add_translation(page_table*, int vir_page, int phys_page);
 
 void remove_translation(page_table *, int vir_page);
 
-bool accessMemory(page_table *, main_mem *, int vir_page, char operation);
+bool readMemory(page_table *, main_mem *, int vir_page, int*);
 
-bool allocateMemory(page_table *, swap_mem *, main_mem *, int vir_page, char operation);
+bool writeMemory(page_table *, main_mem*, int vir_page, int writeData);
 
-void main_to_swap(page_table *, swap_mem *, main_mem *, int index);
+bool allocateMemory(page_table *, node_sm *, main_mem *, int vir_page);
 
-void swap_to_main(page_table *, swap_mem *, main_mem *, int index);
+bool freeMemory(page_table *, node_sm *, main_mem *, int vir_page);
 
-void clear_pages(swap_mem *, main_mem *, int page_amt);
+void swap(page_table *pt, node_sm *head, main_mem *main, int vir_page, int main_index);
 
-int unmodified_pages(main_mem *);
 
 //void run_processes(char * file_name, void(*swag_alg))
-void FIFO_swap(char *file_name, main_mem *, node_sm head);
+void FIFO_swap(char *file_name, main_mem *, node_sm * head);
 
-void LRU_swap(char *file_name, main_mem *, node_sm head);
+void LRU_swap(char *file_name, main_mem *, node_sm * head);
 
-void random_swap(char *file_name, main_mem *, node_sm head);
+void random_swap(char *file_name, main_mem *, node_sm * head);
 
 int main() {
-
-    main_mem *main_memory = (main_mem *) (malloc(sizeof(main_mem)));
-    main_memory->p_id = ((int *) malloc(sizeof(int) * PAGE_AMT));
-    main_memory->vir_page = ((int *) malloc(sizeof(int) * PAGE_AMT));
-    main_memory->pages = ((page_mem *) malloc(sizeof(page_mem) * PAGE_AMT));
+    main_mem *main_memory;
+    node_sm *headSM = NULL;
 
 
-    node_sm * head = NULL;
-    head = malloc(sizeof(node_sm));
-    if (head == NULL) {
-        printf("head malloc failed for swap memory");
-        exit(1);
-    }
-    head->data.p_id = -1;
-    head->next = NULL;
 
-    page_table *pt = (page_table *) (malloc(sizeof(page_table) * PAGE_TABLE_DEFAULT_SIZE));
-
-    for(int i =0; i<PAGE_AMT; i++){
-	    main_memory->p_id[i] = -1;
-    }
-    
 
     char file_name[32] = "memory.dat";
     FILE *f;
@@ -91,12 +102,32 @@ int main() {
     }
     fclose(f);
 
-    // random_swap(file_name, main_memory, head)
+    mainInit(main_memory);
+    swapInit(headSM);
+    printf("Random Swap: \n");
+    random_swap(file_name, main_memory, headSM)
+    mainDealloc(main_memory);
+    swapDealloc(headSM);
+
+    mainInit(main_memory);
+    swapInit(headSM);
+    printf("LRU Swap: \n");
+    // LRU_swap(file_name, main_memory, headSM)
+    mainDealloc(main_memory);
+    swapDealloc(headSM);
+
+    mainInit(main_memory);
+    swapInit(headSM);
+    printf("FIFO Swap: \n");
+    // FIFO_swap(file_name, main_memory, headSM)
+    mainDealloc(main_memory);
+    swapDealloc(headSM);
+
     return 0;
 
 }
 
-void random_swap(char *file_name, main_mem *main_memory, node_sm head) {
+void random_swap(char *file_name, main_mem *main_memory, node_sm * swapHead) {
     char line[100];
     char *token;
     FILE *f;
@@ -108,7 +139,7 @@ void random_swap(char *file_name, main_mem *main_memory, node_sm head) {
     while (fgets(line, 100, f)) {
         token = strtok(line, " \t\n");
         incrementer = 0;
-	v_id = -1;
+        v_id = -1;
         while (token != NULL) {
             if (incrementer == 0) {
                 p_id = atoi(token);
@@ -119,41 +150,89 @@ void random_swap(char *file_name, main_mem *main_memory, node_sm head) {
             } else {
                 v_id = atoi(token);
             }
-           
+
             token = strtok(NULL, " \t\n");
         }
 
-	//process the line 
+        //process the line
 
-	//check if the operation is process creation or termination
-	if(v_id == -1){
-		//check if operation is creation of process
-		if(operation == 'C'){
-			//create page table for process
-			
-		}
-		else{
+        //check if the operation is process creation or termination
+        if(v_id == -1){
+            //check if operation is creation of process
+            if(operation == 'C'){
+                //create page table for process
+                for(int i =0; i<SWAP_MEMORY_AMT; i++){
+                    if(pt[i].p_id != -1){
+                        pt[i].p_id = p_id;
+                    }
+                }
+            }
+            else{
+                for(int i = 0; i<PAGE_AMT; i++){
+                    if(main_memory->p_id[i] == p_id){
+                        main_memory->p_id[i] = -1;
+                        main_memory->pages[i].data =-1;
+                        main_memory->pages[i].dirty =false;
+                        main_memory->pages[i].accessed = false;
 
-		}
-	}
+                    }
+                }
+                for(int i = 0; i<SWAP_MEMORY_AMT; i++){
+                    if(pt[i].p_id = p_id){
+                        pt[i].p_id = -1;
+                        pt[i].valid = false;
+                        pt[i].present = false;
+                    }
+                }
+            }
+        }
+        else{
+            if(operation == 'W' || operation == 'R'){
+                bool access = accessMemory(pt,main_memory,v_id,operation);
+                if (access){
+                    continue;
+                }
+            }
+            else if(operation == 'A' || operation == 'F'){
+                bool success = allocateMemory(pt, swap_memory,main_memory,v_id, operation);
+                if(success){
+                    continue;
+                }
+
+                else{
+                    //swap function
+                    //find random page number in main memory
+                    int num = (rand() % 20);
+                    main_to_swap(pt,swap_memory,main_memory,num);
+                    main_memory->p_id[num] = p_id;
+                    main_memory->vir_page = v_id;
+
+                }
+            }
+
+        }
 
     }
 }
 
 //only use for reading/writing
-bool accessMemory(page_table *page_tb, main_mem *main_m, int vir_page, char operation) {
-    if (!(page_tb->present[vir_page] && page_tb->valid[vir_page]))
+bool readMemory(page_table *page_tb, main_mem *main_m, int vir_page, int *readData) {
+    node_t * current = page_tb->head;
+    while( (current->next != NULL) && (current->data.virtualPage != vir_page) ){
+        current = current->next;
+    }
+    if( (current->next == NULL) && (current->data.virtualPage != vir_page) ){
         return false;
-    if (operation == 'R') {
-        //just read the data, no need to do anything with it
-        int temp = main_m->pages[page_tb->translations[vir_page]].data;
-	main_m->pages[page_tb->translations[vir_page]].accessed = true;
+    }
+    if ( !(current->data.valid) ) {
+        return false;
+    }
+    if(current->data.present){ //in main mem
+        *readData = main_m->data[current->data.physicalPage];
+        main_m->accessed[current->data.physicalPage] = true;
         return true;
-    } else if (operation == 'W') {
-        main_m->pages[page_tb->translations[vir_page]].data = 1;
-        main_m->pages[page_tb->translations[vir_page]].dirty = true;
-        return true;
-    } else return false;
+    }
+    return false;
 }
 
 //only use for allocating and deallocate memory
@@ -163,72 +242,51 @@ bool accessMemory(page_table *page_tb, main_mem *main_m, int vir_page, char oper
 
 //TODO: CHECK PAGE TABLE FOR VALID TRANSLATION
 
-bool allocateMemory(page_table *page_tb, swap_mem *swap_m, main_mem *main_m, int vir_page, char operation) {
+bool allocateMemory(page_table *page_tb, node_sm * swapHead, main_mem *main_m, int vir_page) {
+    node_t * current = page_tb->head;
     int index = -1;
-    if (operation == 'A') {
-        for (int i = 0; i < PAGE_AMT; i++) {
-            if (main_m->p_id[i] == -1) {
-                main_m->p_id[i] = page_tb->p_id;
-                main_m->vir_page[i] = vir_page;
-                main_m->pages[i].dirty = false;
-                main_m->pages[i].data = 0;
-                add_translation(page_tb, vir_page, i);
-                return true;
-            } else if (!(main_m->pages[i].dirty)) {
-                index = i;
-            }
+    for(int i = 0; i != PAGE_AMT; ++i){
+        if(main_m->p_id[i] == -1){
+            index = main_m->p_id[i];
+            break;
         }
-        if (index > -1) {
-            main_to_swap(page_tb, swap_m, main_m, index);
-            main_m->p_id[index] = page_tb->p_id;
-            main_m->vir_page[index] = vir_page;
-            main_m->pages[index].dirty = false;
-            main_m->pages[index].data = 0;
-            add_translation(page_tb, vir_page, index);
-        } else return false;
-    } else if (operation == 'F') {
-        if(!(page_tb->valid[vir_page]))
-            return false;
-        for (int i = 0; i < PAGE_AMT; i++) {
-            if (main_m->p_id[i] == page_tb->p_id) {
-                main_m->p_id[i] = -1;
-                main_m->vir_page[i] = -1;
-                main_m->pages[i].dirty = false;
-                main_m->pages[i].data = 0;
-                remove_translation(page_tb,vir_page);
-                return true;
-            }
-        }
-        for (int i = 0; i < PAGE_TABLE_DEFAULT_SIZE; i++) {
-            if (swap_m->p_id[i] == page_tb->p_id) {
-                swap_m->p_id[i] = -1;
-                swap_m->vir_page[i] = -1;
-                swap_m->pages[i].dirty = false;
-                swap_m->pages[i].data = 0;
-                remove_translation(page_tb,vir_page);
-                return true;
-            }
-        }
-        return false;
     }
-
+    if(index == -1){ //main mem full
+        swap()
+    }
+    else{
+        main_m->p_id[index] = page_tb->p_id;
+        main_m->data[index] = 0;
+        main_m->inSwap[index] = false;
+        main_m->dirty[index] = false;
+        main_m->accessed[index] = false;
+    }
 }
 
-//assumes you checked that all pages are taken first
-int unmodified_pages(main_mem *mem ){
-	int i = 0;
-	for(i; i<20; i++){
-		//tests if page has been modified 
-		if(mem->pages[i].dirty ==false && mem->pages[i].accessed == false){
-			//return page number if page is unmodified 
-			return i;
-		}
-
-		else{
-			return -1;
-		}
-	}
-	
+bool freeMemory(page_table *page_tb, node_sm *swapHead, main_mem *main_m, int vir_page){
+    if(!(page_tb->valid[vir_page]))
+        return false;
+    for (int i = 0; i < PAGE_AMT; i++) {
+        if (main_m->p_id[i] == page_tb->p_id) {
+            main_m->p_id[i] = -1;
+            main_m->vir_page[i] = -1;
+            main_m->pages[i].dirty = false;
+            main_m->pages[i].data = 0;
+            remove_translation(page_tb,vir_page);
+            return true;
+        }
+    }
+    for (int i = 0; i < PAGE_TABLE_DEFAULT_SIZE; i++) {
+        if (swap_m->p_id[i] == page_tb->p_id) {
+            swap_m->p_id[i] = -1;
+            swap_m->vir_page[i] = -1;
+            swap_m->pages[i].dirty = false;
+            swap_m->pages[i].data = 0;
+            remove_translation(page_tb,vir_page);
+            return true;
+        }
+    }
+    return false;
 }
 
 void add_translation(page_table *pt,int vir_page, int phys_page){
@@ -241,45 +299,82 @@ void remove_translation(page_table *pt, int vir_page){
 	return;
 }
 
-void main_to_swap(page_table *pt, swap_mem *swap, main_mem *main,int index){
-	
-	int swap_index;
+void swap_to_main(process_list *headPL, main_mem *main, int PID_leaving, int PID_arriving, int main_index){
+    int tmpData = main->data[main_index];
+    bool tmpDirty = main->dirty[main_index];
+    bool tmpInSwap = main->inSwap[main_index];
 
-	//find a free page in swap memory 
-	for(int i=0; i<1000; i++){
-		if(swap->p_id[i] == -1){
-			swap_index = i;
-			break;
-		}
+    //get the values out of swap and into main mem
+    node_sm * current = headSM;
+    while( (current->next != NULL) && (current->data.p_id != vir_page) ){
+        current = current->next;
+    }
+    main->data[main_index] = current->data.data;
+    main->dirty[main_index] = false;
+    main->accessed[main_index] = false;
+    main->inSwap[main_index] = true;
 
-	}
-
-
-	//copy contents to swap memory
-	swap->p_id[swap_index] = main->p_id[index];
-	swap->vir_page[swap_index] = main->vir_page[index];
-	swap->pages[swap_index].data = main->pages[index].data;
-	swap->pages[swap_index].dirty = main->pages[index].dirty;
-	swap->pages[swap_index].accessed = main->pages[index].accessed;
-
+    if(tmpInSwap){ //There is an old page
+        if(tmpDirty) { //Need to find the old page and replace data
+            node_sm * oldpage = head;
+            while ((oldpage->next != NULL)) {
+                oldpage = oldpage->next;
+            }
+            oldpage->data.data = tmpData;
+        }
+    }
+    else { //No old page, just create a new entry in swap linked list
+        node_sm * cur = head;
+        while ((cur->next != NULL)) {
+            cur = cur->next;
+        }
+        cur->next = malloc(sizeof(node_sm));
+        cur->next->data.data = tmpData;
+        cur->next->next = NULL;
+    }
 }
 
-void swap_to_main(page_table *pt, swap_mem *swap, main_mem *main, int index){
-	int main_index;
-	
-	//find free page in main memory
-	for(int i = 0; i<20; i++){
-		if(main->p_id[i] == -1){
-			main_index = i;
-			break;
-		}
-	}
-	
-	//copy contents to main memory
-	main->p_id[main_index] = swap->p_id[index];
-	main->vir_page[main_index] = swap->vir_page[index];
-	main->pages[main_index].data = swap->pages[index].data;
-	main->pages[main_index].dirty = swap->pages[index].dirty;
-	main->pages[main_index].accessed = swap->pages[index].accessed;
+void mainInit(main_mem * main){
+    main = (main_mem *) (malloc(sizeof(main_mem)));
+    main->p_id = ((int *) malloc(sizeof(int) * PAGE_AMT));
+    main->vir_page = ((int *) malloc(sizeof(int) * PAGE_AMT));
+    main->data = ((int *) malloc(sizeof(int) * PAGE_AMT));
+    main->dirty = ((bool *) malloc(sizeof(bool) * PAGE_AMT));
+    main->accessed = ((bool *) malloc(sizeof(bool) * PAGE_AMT));
+    main->inSwap = ((bool *) malloc(sizeof(bool) * PAGE_AMT));
+    for(int i = 0; i != PAGE_AMT; ++i){
+        main->p_id[i] = -1;
+        main->vir_page[i] = -1;
+        main->data[i] = 0;
+        main->dirty[i] = false;
+        main->accessed[i] = false;
+        main->inSwap[i] = false;
+    }
+}
 
+void swapInit(node_sm *head){
+    head = malloc(sizeof(node_sm));
+    head->data.p_id = -1;
+    head->next = NULL;
+}
+
+void mainDealloc(main_mem* main){
+    free(main->p_id);
+    free(main->vir_page);
+    free(main->data);
+    free(main->dirty);
+    free(main->accessed);
+    free(main->inSwap);
+    free(main);
+}
+
+void swapDealloc(node_sm* head){
+    node_sm * current = head;
+    node_sm * delete;
+    while(current->next != NULL){
+        delete = current;
+        current = current->next;
+        free(delete);
+    }
+    free(current);
 }
